095c79e366d5b80d2d5860bb54939626f016ccc9
Egor Z <egor.zevakin.94@gmail.com>
2023-03-18 02:07:34 +0300

    add put user handle

--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from 'express'
 import User from '../../models/userModel';
 import { faker } from '@faker-js/faker';
+import mongoose from 'mongoose';
 
 
 const router = Router()
@@ -9,6 +10,24 @@ router.get('/users', async (res: Response) => {
   const users = await User.find({})
   return res.status(200).send(users)
 })
+
+router.put('/users/:id', async (req: Request, res: Response) => {
+  const userId = new mongoose.Types.ObjectId(req.params.id);
+  const user = await User.findById(userId);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.phone = req.body.phone || user.phone;
+    const updatedUser = await user.save();
+    const { _id, name, email, phone } = updatedUser
+    res.send({
+      _id,
+      name,
+      email,
+      phone,
+    });
+  }
+});
 
 router.post('/users', async (req: Request) => {
   const { name, email, phone } = req.query;
