bf4d6e7f0293ab5e268d96fdae999bed0166c8d7
Egor Z <egor.zevakin.94@gmail.com>
2023-03-21 18:04:52 +0300

    add fetcher

---
+++ frontend/src/utils/fetcher.ts
@@ -0,0 +1,9 @@
+export const fetcher = async (url: string) => {
+  const baseUrl = import.meta.env.VITE_API_URL
+  const response = await fetch(`${baseUrl}/${url}`)
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+  return await response.json()
+}
