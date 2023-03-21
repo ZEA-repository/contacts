01090381f63165d12fe22b82dadcff92ab0ebe00
Egor Z <egor.zevakin.94@gmail.com>
2023-03-21 17:57:23 +0300

    add router v6

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,27 @@
 import MainLayout from '@/layouts/Main'
-import { Routes, Route } from 'react-router-dom'
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom'
 import { HomePage } from '@/pages/home'
 import { ContactsPage } from '@/pages/contacts'
-import { BrowserRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 import { ThemeProvider } from '@/ThemeProvider'
+import { fetchUsers } from '@/api'
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path='contacts' element={<ContactsPage />} loader={fetchUsers} />
+    </Route>
+  )
+)
 export default function App() {
   return (
     <ThemeProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<MainLayout />}>
-            <Route index element={<HomePage />} />
-            <Route path=':slug' element={<ContactsPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </ThemeProvider>
   )
 }
