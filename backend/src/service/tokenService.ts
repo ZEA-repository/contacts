d88463b35abafe008465697e589c7f5b71c4c762
Egor Z <egor.zevakin.94@gmail.com>
2023-04-04 21:29:23 +0300

    add remove token, find token, validate token

--- backend/src/service/tokenService.ts
+++ backend/src/service/tokenService.ts
@@ -1,6 +1,6 @@
 import TokenModel from '~/models/tokenModel';
 const jwt = require('jsonwebtoken');
-
+const ApiError = require('~/exceptions/apiError')
 interface Payload {
   email: string
   id: string
@@ -9,7 +9,7 @@ interface Payload {
 
 class TokenService {
   async generateTokens(payload: Payload) {
-    const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '30m' })
+    const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '30s' })
     const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: '30d' })
     return {
       accessToken, refreshToken
@@ -26,6 +26,36 @@ class TokenService {
     const token = await TokenModel.create({ user: userId, refreshToken })
     return token
   }
+
+  async removeToken(refreshToken: string) {
+    const tokenData = await TokenModel.deleteOne({ refreshToken })
+    return tokenData
+  }
+
+  async findToken(refreshToken: string) {
+    const tokenData = await TokenModel.findOne({ refreshToken })
+    return tokenData
+  }
+
+  validateAccessToken(token: string) {
+    try {
+      const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET)
+      return userData
+    } catch (e) {
+      return null
+    }
+  }
+  validateRefreshToken(token: string) {
+    try {
+      const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET)
+      return userData
+    } catch (e) {
+      return null
+    }
+  }
+
+
+
 }
 
 module.exports = new TokenService()
