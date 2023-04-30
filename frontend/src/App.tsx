e8f31b7e29c91c667ebf6f1826359c4f37ccf625
Egor Z <egor.zevakin.94@gmail.com>
2023-05-01 00:24:44 +0300

    add login, registration routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,32 @@
 import MainLayout from '@/layouts/Main'
+import EmptyLayout from '@/layouts/Empty'
 import {
   Route,
   createBrowserRouter,
   createRoutesFromElements,
 } from 'react-router-dom'
-import { HomePage } from '@/pages/home'
+import { LoginPage } from '@/pages/login'
+import { RegistrationPage } from '@/pages/registration'
 import { ContactsPage } from '@/pages/contacts'
 import { RouterProvider } from 'react-router-dom'
 import { ThemeProvider } from '@/ThemeProvider'
-import { fetchUsers } from '@/api'
+// import { fetchUsers } from '@/api'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path='contacts' element={<ContactsPage />} loader={fetchUsers} />
-    </Route>
+    <>
+      <Route path='/' element={<MainLayout />}>
+        <Route
+          index
+          element={<ContactsPage />}
+          // loader={fetchUsers}
+        />
+      </Route>
+      <Route path='/' element={<EmptyLayout />}>
+        <Route path='login' element={<LoginPage />} />
+        <Route path='registration' element={<RegistrationPage />} />
+      </Route>
+    </>
   )
 )
 export default function App() {
